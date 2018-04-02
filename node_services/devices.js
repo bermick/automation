496dc1c805cfa7d51cf9f3ca8d4323660fcff086
Bernardo Mtz <bernardo@barrafrancamexico.com>
2018-04-02 21:58:25 +0100

    do not throw error

--- node_services/devices.js
+++ node_services/devices.js
@@ -1,32 +1,28 @@
 const db = require('./db-module')
 
 async function getStagedDevice(device) {
-	try {
-		let previouslyAdded = await db.executeGenericQuery('SELECT * FROM Devices where Devices.code = "' + device.key + '"');
-		if (previouslyAdded.length) {
-			throw 'Device has already been added';
-		}
+	let previouslyAdded = await db.executeGenericQuery('SELECT * FROM Devices where Devices.code = "' + device.key + '"');
+	if (previouslyAdded.length) {
+		return 'Device has already been added';
+	}
 
-		if (isDeviceStaged(device.key)) {
-			// test if device is a valid device, i.e. has an existing key in mac_keys
-			let registeredDevices = await db.executeGenericQuery('SELECT * FROM mac_keys WHERE mac_keys.key = "' + device.key + '"');
-			if(registeredDevices.length === 1) {
-				let userID = await db.executeGenericQuery('SELECT Users.id FROM Users WHERE Users.email = "' + device.email + '" LIMIT 1');
-				let insertQuery = 'INSERT INTO Devices (id,mac_address,state,user_id,code,name) VALUES(null,"' + registeredDevices[0].mac_address +'", "0", "' + userID[0].id  + '", "' + device.key + '", "' + device.name + '" )';
-				let insertResult = await db.executeGenericQuery(insertQuery);
-				if (insertResult.affectedRows) {
-					return 'Device ' + device.key + ' added successfully';
-				} else {
-					return 'There was an error adding device ' + device.key + ', please try again.'
-				}
+	if (isDeviceStaged(device.key)) {
+		// test if device is a valid device, i.e. has an existing key in mac_keys
+		let registeredDevices = await db.executeGenericQuery('SELECT * FROM mac_keys WHERE mac_keys.key = "' + device.key + '"');
+		if(registeredDevices.length === 1) {
+			let userID = await db.executeGenericQuery('SELECT Users.id FROM Users WHERE Users.email = "' + device.email + '" LIMIT 1');
+			let insertQuery = 'INSERT INTO Devices (id,mac_address,state,user_id,code,name) VALUES(null,"' + registeredDevices[0].mac_address +'", "0", "' + userID[0].id  + '", "' + device.key + '", "' + device.name + '" )';
+			let insertResult = await db.executeGenericQuery(insertQuery);
+			if (insertResult.affectedRows) {
+				return 'Device ' + device.key + ' added successfully';
 			} else {
-				throw 'Please verify your device key and try again';
+				return 'There was an error adding device ' + device.key + ', please try again.'
 			}
 		} else {
-			throw 'No staging device for ' + device.key;
+			return 'Please verify your device key and try again';
 		}
-	} catch(err) {
-		return err;
+	} else {
+		return 'No staging device for ' + device.key;
 	}
 }
 
@@ -38,6 +34,7 @@ async function isDeviceStaged(key) {
 async function getUser(email) {
 	try	{
 		let userID = await db.executeGenericQuery('SELECT * FROM Users WHERE Users.email = "' + email + '" LIMIT 1');
+		console.log(userID);
 		if (userID.length === 1) {
 			return userID[0];
 		}
@@ -50,9 +47,8 @@ async function getUser(email) {
 
 async function getDevicesForUser(email) {
 	try {
-		let user = await getUser(email);
-		let userDevices = await db.executeGenericQuery('SELECT * FROM Devices WHERE Devices.user_id = ' + user.id);
-		return userDevices;
+		let user = await getUser(email); console.log(user);
+		return db.executeGenericQuery('SELECT * FROM Devices WHERE Devices.user_id = ' + user.id);
 	} catch(error) {
 		throw 'Error getting devices for user: ' + email;
 	}
